1f676a6c0685dad15fd8ec256168849812350365
Yu <echo.aimless@gmail.com>
2026-07-09 10:49:10 +0800

    chore: prepare for version 0.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, str(ROOT))
 project = 'mlx-lattice'
 author = 'Z.Y. Lin'
 copyright = f'{datetime.now(UTC).year}, {author}'
-release = '0.3.0'
+release = '0.3.1'
 version = release
 
 extensions = [
--- tests/ir/test_contract_package.py
+++ tests/ir/test_contract_package.py
@@ -18,7 +18,7 @@ def test_lattice_contract_imports_without_mlx_lattice_runtime() -> None:
     assert contract.ARTIFACT_WEIGHT_FILE == 'weights.safetensors'
     assert contract.CURRENT_DIALECT_VERSION == 0
     assert contract.LATTICE_DIALECT.namespace == 'lattice'
-    assert contract.__version__ == '0.3.0'
+    assert contract.__version__ == '0.3.1'
     assert 'mlx_lattice' not in sys.modules
 
 
